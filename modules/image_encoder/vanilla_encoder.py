e886ecf8e2cd425a983d87d3ebd6b22197b26ab7
Tianxiang Gao <gtx9726@gmail.com>
2019-04-23 19:46:06 -0400

    change vanilla encoder

--- modules/image_encoder/vanilla_encoder.py
+++ modules/image_encoder/vanilla_encoder.py
@@ -1,13 +1,28 @@
 import torch.nn as nn
 import torchvision.models as models
+import torch.nn.functional as F
 
 
 class CNN(nn.Module):
-    def __init__(self, load_pretrained=False):
+    def __init__(self, hps, load_pretrained=False):
         super().__init__()
+        self.hps = hps
         self.inception = models.inception_v3(pretrained=load_pretrained)
+        self.features = nn.Sequential(*list(self.inception.children())[0:-1])
+        self.dropout = nn.Dropout()
 
     def forward(self, imgs):
+        """
+
+        Args:
+            imgs: batch of images N * H * W * C
+
+        Returns:
+            x: batch of feature vectors N * 2048
+        """
         x = imgs
-        x = self.inception(x)
+        x = self.features(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = self.dropout(x)
+        x = x.view(x.size(0), -1)
         return x
